3407e6a421ca51e8dea9b70370eaeb81174c8aac
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-04-13 08:29:18 -0600

    Removed unused extra methods.

--- src/calculator/controller/Controller.java
+++ src/calculator/controller/Controller.java
@@ -12,38 +12,4 @@ public class Controller
 	{
 		appFrame = new Frame(this);
 	}
-	
-	public boolean isValidInteger(String input)
-	{
-		boolean valid = false;
-		
-		try
-		{
-			Integer.parseInt(input);
-			valid = true;
-		}
-		catch (NumberFormatException error)
-		{
-			JOptionPane.showMessageDialog(appFrame, "Only integer values are valid: " + input + " is not.");
-		}
-		
-		return valid;
-	}
-	
-	public boolean isValidDouble(String input)
-	{
-		boolean valid = false;
-		
-		try
-		{
-			Double.parseDouble(input);
-			valid = true;
-		}
-		catch (NumberFormatException error)
-		{
-			JOptionPane.showMessageDialog(appFrame, "Only double values are valid: " + input + " is not.");
-		}
-		
-		return valid;
-	}
 }
